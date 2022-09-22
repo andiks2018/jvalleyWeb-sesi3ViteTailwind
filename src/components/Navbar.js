becc6a6cf6b89f72884d4edbb8eda70ba897237a
andi <andiks2018@gmail.com>
2022-09-23 03:51:55 +0700

    sudah kelar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,15 @@
+window.handleLogout = () => {
+  // clear localStorage data
+  localStorage.clear();
 
-window.handleLogout = ()=>{
-    // clear localStorage data
-    localStorage.clear();
+  // kembalikan user ke login page
+  window.location.href = "/login";
+};
 
-    // kembalikan user ke login page
-    window.location.href = '/login'
-}
+export default function Navbar() {
+  let userData = localStorage.getItem("userData");
 
-
-export default function Navbar(){
-
-    let userData = localStorage.getItem('userData');
-
-    return `
+  return `
     <nav class="w-full h-20 flex bg-orange-500">
         <div class="w-full max-w-[1440px] flex items-center justify-start px-4 mx-auto">
 
@@ -22,23 +19,27 @@ export default function Navbar(){
                 Jvalley Blogs
             </h1> 
             
-           ${ userData ? `
+           ${
+             userData
+               ? `
             <menu class="flex gap-4 text-white font-light ml-auto items-center">
                 <a href="/" >Home</a>
                 <a href="/addBlog" >Add Blog</a>
                 <a onclick="handleLogout()" class="cursor-pointer">Logout</a>
                 <span class="px-4 py-2 bg-orange-600 rounded-full">
-                    ${ JSON.parse(userData).user.email }
+                    ${JSON.parse(userData).user.email}
                 </span>
-            </menu>` : `
+            </menu>`
+               : `
             <menu class="flex gap-4 text-white font-light ml-auto">
                 <a href="/" >Home</a>
                 <a href="/register" >Register</a>
                 <a href="/login" >Login</a>
             </menu>
-            ` }
+            `
+           }
 
         </div>
     </nav>
-    `
+    `;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,49 @@
 import axios from "axios";
 import Navbar from "../components/Navbar";
 const app = document.getElementById('app');
-
 export default function Home(){
-
-    // ubah document title
-    document.title = "Home Page";
-
-    axios.get('http://localhost:3000/blogs')
-    .then(res => {
-        app.innerHTML += Navbar()
+    //ubah document title
+    document.title= "Home Page";
+    //mendapatkan document title
+    axios.get("http://localhost:3000/blogs?_sort=id&_order=desc") 
+    .then((res)=>{
+        app.innerHTML += Navbar();
         res.data.forEach((e)=>{
-            app.innerHTML += `
-                <div class="flex flex-col gap-4 max-w-[900px] bg-white shadow-md p-6 my-4 mx-auto">
-                    <h1>${e.judul}</h1>
+            app.innerHTML +=`
+                <a href="/details?id=${e.id}" class="bg-white p-6 
+                flex flex-col rounded-md my-6 max-w-[700px] shadow-md">
+                <div class="bg-white p-6 flex flex-col 
+                rounded-md my-6 max-w-[700px] shadow-md">
+                    <h1 class="text-2xl">${e.judul}</h1>
                     <small>${e.author}</small>
-                    <img src="${e.img}" alt="${e.judul}" class="w-full h-[280px] object-cover"/>
+                    <img src="${e.img}" alt="${e.judul}" 
+                    class="h-[280px] object-cover my-4 rounded-lg" />
                     <p>${e.content}</p>
-                </div>
+                <div>
             `
         })
     })
+    .catch((err)=>{
+        console.error(err);
+    })
 }
+
+
+//     // ubah document title
+//     document.title = "Home Page";
+
+//     axios.get('http://localhost:3000/blogs')
+//     .then(res => {
+//         app.innerHTML += Navbar()
+//         res.data.forEach((e)=>{
+//             app.innerHTML += `
+//                 <div class="flex flex-col gap-4 max-w-[900px] bg-white shadow-md p-6 my-4 mx-auto">
+//                     <h1>${e.judul}</h1>
+//                     <small>${e.author}</small>
+//                     <img src="${e.img}" alt="${e.judul}" class="w-full h-[280px] object-cover"/>
+//                     <p>${e.content}</p>
+//                 </div>
+//             `
+//         })
+//     })
+// }
